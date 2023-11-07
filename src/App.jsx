acbae8f335cc66cf7534dd1263469155ae21989c
Aagneyan <aagneyan.namboodiri@infilect.com>
2023-11-07 15:06:56 +0530

    Added infiviz.in as iframe

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 const App = () => (
     <>
       <h1>Infiviz as an iFrame</h1>
-      <iframe width="1000" height="700" src="https://pg-indo.infiviz.me/app/store-explorer-v2?q=e5f1cf61-871b-4bf7-9413-ee0e96874255" allowFullScreen></iframe>
+      <iframe width="1000" height="700" src="https://infiviz.in/" allowFullScreen></iframe>
       {/* https://2b54-106-51-84-218.ngrok-free.app */}
       {/* <iframe width="700" height="700" src="https://2b54-106-51-84-218.ngrok-free.app"></iframe> */}
       {/* <button onClick={() => document.cookie = "username=John Doe; SameSite=None; Secure"}>Set cookie</button> */}
