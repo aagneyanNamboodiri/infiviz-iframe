ce5b8af545af45055bcd26b7ae21cd04c6e1d217
Aagneyan <aagneyan.namboodiri@infilect.com>
2023-11-09 11:06:08 +0530

    Removed iFrame border

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import React from "react"
+import './App.css'
 
 const App = () => (
     <>
       <h1>Infiviz as an iFrame</h1>
-      <iframe width="1000" height="750" src="https://infiviz.in/" allowFullScreen></iframe>
+      <iframe
+        style={{border: "0"}} 
+        width="1000" 
+        height="750" 
+        src="https://infiviz.in/" 
+        allowFullScreen />
       {/* https://2b54-106-51-84-218.ngrok-free.app */}
       {/* <iframe width="700" height="700" src="https://2b54-106-51-84-218.ngrok-free.app"></iframe> */}
       {/* <button onClick={() => document.cookie = "username=John Doe; SameSite=None; Secure"}>Set cookie</button> */}
